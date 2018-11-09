4b57111dc3495be350407fab5e05b477ba475aac
jweatherby <jweatherby@gadventures.com>
2018-11-09 14:42:32 -0500

    Re-add missing header style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,12 @@
 import React from "react";
 
+const headerStyle = {
+    fontFamily: 'Montserrat, Sans-serif',
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#4c4f56',
+}
+
 const Header = ({passenger}) =>
     <h2 style={headerStyle}>
         {passenger ? `${passenger}, do` : "Do"} you need a visa?
